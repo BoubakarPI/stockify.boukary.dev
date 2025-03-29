5b3b7ab4e900bd0d4a371efd75b89aec7c51a606
BoubakarPI <agenceweb1919@gmail.com>
2025-03-29 06:42:55 +0000

    feat(inventory): add Product model & make migration

--- src/inventory/models.py
+++ src/inventory/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    stock = models.PositiveIntegerField(default=0)
+    thumbnail = models.ImageField(upload_to="products", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
