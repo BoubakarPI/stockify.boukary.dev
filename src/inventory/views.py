cc244957b4d2fbbba88ad873a9d5f263d32f8e52
BoubakarPI <agenceweb1919@gmail.com>
2025-03-29 08:26:42 +0000

    feat(inventory): init inventory/urls.py

--- src/inventory/views.py
+++ src/inventory/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Product
+
 
 # Create your views here.
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "inventory/product_list.html"
