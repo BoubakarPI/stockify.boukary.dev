34d70dd36232e9eaa1454ad743775029d41679b0
BoubakarPI <agenceweb1919@gmail.com>
2025-03-30 16:44:42 +0000

    chore(inventory): serve static files through /content/

--- src/stockify/settings.py
+++ src/stockify/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import MEDIA_URL, MEDIA_ROOT
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -125,3 +127,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "accounts.UserModel"
+MEDIA_URL = "/content/"
+MEDIA_ROOT = BASE_DIR / "medias"
