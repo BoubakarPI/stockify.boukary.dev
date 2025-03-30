f2d7035f246c50a4f5488ffbcd9b472421c6c3a7
BoubakarPI <agenceweb1919@gmail.com>
2025-03-30 20:37:19 +0000

    feat(inventory): add ProductDetail MVT

--- src/inventory/views.py
+++ src/inventory/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from .models import Product
 
-
-# Create your views here.
 
 class ProductListView(ListView):
     model = Product
     template_name = "inventory/product_list.html"
 
+
+class ProductDetailView(DetailView):
+    template_name = 'inventory/product.html'
+    model = Product
+    context_object_name = 'product'
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Product, slug=self.kwargs['slug'])
