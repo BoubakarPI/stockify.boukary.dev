54c58f08e5da90335652f43a2c86ddac67cbfde9
BoubakarPI <agenceweb1919@gmail.com>
2025-03-30 20:35:56 +0000

    chore(inventory): add slug field in Product model

--- src/inventory/models.py
+++ src/inventory/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
+    slug = models.SlugField(max_length=125)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
     stock = models.PositiveIntegerField(default=0)
     thumbnail = models.ImageField(upload_to="products", blank=True, null=True)
