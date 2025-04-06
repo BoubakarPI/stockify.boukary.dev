0f4a5d17a1a1c87fc2558a92d800f7b85e0ad631
BoubakarPI <agenceweb1919@gmail.com>
2025-04-06 11:40:24 +0000

    feat(inventory): dynamise sidebar active nav in layout

--- src/inventory/urls.py
+++ src/inventory/urls.py
@@ -3,9 +3,10 @@ from .views import ProductListView, ProductDetailView, ProductCreateView, Produc
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='products'),
+    path('orders', ProductListView.as_view(), name='orders'),
     path('product/<str:slug>/', ProductDetailView.as_view(), name="product_detail"),
-    path('products/add/', ProductCreateView.as_view(), name='product_add'),
-    path('products/<int:pk>/edit/', ProductUpdateView.as_view(), name='product_edit'),
-    path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
+    path('add/', ProductCreateView.as_view(), name='product_add'),
+    path('<int:pk>/edit/', ProductUpdateView.as_view(), name='product_edit'),
+    path('<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 
 ]
--- src/stockify/urls.py
+++ src/stockify/urls.py
@@ -19,9 +19,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 from . import settings
+from .views import index
 
 urlpatterns = [
-    path('', include('inventory.urls')),
+    path('', index, name="index"),
+    path('products/', include('inventory.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
