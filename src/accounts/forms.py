9561887748f9f1ef9d46c7d422d59b410057a201
BoubakarPI <agenceweb1919@gmail.com>
2025-04-07 16:34:12 +0000

    chore(accounts): Stylise auth-layout with bootstrap

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -4,19 +4,19 @@ from .models import User
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True, label="Addresse e-mail", widget=forms.EmailInput(attrs={
-            'class': 'border border-gray-300 rounded-lg p-2 w-full focus:ring-2 focus:ring-blue-500 focus:outline-none'
+            'class': 'form-control'
         }))
     fullname = forms.CharField(max_length=120, min_length=5, required=True, label="Nom Prénom", widget=forms.TextInput(attrs={
-            'class': 'border border-gray-300 rounded-lg p-2 w-full focus:ring-2 focus:ring-blue-500 focus:outline-none'
+            'class': 'form-control'
         }))
     password1 = forms.CharField(required=True, label="Mot de passe",
         widget=forms.PasswordInput(attrs={
-            'class': 'border border-gray-300 rounded-lg p-2 w-full focus:ring-2 focus:ring-blue-500 focus:outline-none'
+            'class': 'form-control'
         })
     )
     password2 = forms.CharField(required=True, label="Confirmer votre mot de passe",
         widget=forms.PasswordInput(attrs={
-            'class': 'border border-gray-300 rounded-lg p-2 w-full focus:ring-2 focus:ring-blue-500 focus:outline-none'
+            'class': 'form-control'
         })
     )
 
@@ -26,8 +26,8 @@ class CustomUserCreationForm(UserCreationForm):
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.EmailField(required=True, label="Addresse e-mail", widget=forms.EmailInput(attrs={
-            'class': 'border border-gray-300 rounded-lg p-2 w-full focus:ring-2 focus:ring-blue-500 focus:outline-none'
+            'class': 'form-control'
         }))
-    password = forms.CharField(required=True, label="Mot de passe", widget=forms.PasswordInput(attrs={'class': 'border border-gray-300 rounded-lg p-2 w-full focus:ring-2 focus:ring-blue-500 focus:outline-none'
+    password = forms.CharField(required=True, label="Mot de passe", widget=forms.PasswordInput(attrs={'class': 'form-control'
                                 })
                                 )
