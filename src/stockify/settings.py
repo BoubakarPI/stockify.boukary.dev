16d872c8ad5f4dee89223e6055a6f6c98281d61f
Boukary DIALLO <163854543+BoubakarPI@users.noreply.github.com>
2025-04-08 15:16:40 +0000

    Update settings.py

--- src/stockify/settings.py
+++ src/stockify/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-q#1%4d)0ryq1yp_wjh85fl*!+wzai&jmlo#gpwtz1*ebdxj#p@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '192.168.11.112']
+ALLOWED_HOSTS = []
 
 
 # Application definition
